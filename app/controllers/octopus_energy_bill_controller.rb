b5eb61b79167f3cab93a12d785c70ac6274fbd69
kimkim0814 <kimura.tsubasa@a-tm.co.jp>
2023-10-12 01:15:31 +0900

    Enable to send line notification

--- app/controllers/octopus_energy_bill_controller.rb
+++ app/controllers/octopus_energy_bill_controller.rb
@@ -35,11 +35,27 @@ class OctopusEnergyBillController < ApplicationController
       fromDatetime: Date.yesterday.beginning_of_day.iso8601,
       toDatetime: Date.yesterday.end_of_day.iso8601
       })
+
     properties = result.original_hash.dig("data", "account", "properties")
     electricity_supply_points = properties.first["electricitySupplyPoints"]
     half_hourly_readings = electricity_supply_points.first["halfHourlyReadings"]
     @kwh = half_hourly_readings.pluck("value").map(&:to_f).sum
     @cost = half_hourly_readings.pluck("costEstimate").map(&:to_f).sum
-    puts "#{Date.yesterday.strftime('%Y年%m月%d日')}は#{@kwh.round(2)}kWh消費して#{@cost}円かかったよ" 
+    text = "#{Date.yesterday.strftime('%Y年%m月%d日')}は#{@kwh.round(2)}kWh消費して#{@cost}円かかったよ" 
+
+		message = {
+      type: 'text',
+      text: text
+    }
+    client.broadcast(message)
   end
+
+	private
+
+	def client 
+		@client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+	end
 end
